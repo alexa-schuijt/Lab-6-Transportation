e21648d757e50d19b4a6afd7a981dcd5e69325ec
Racheljoness <91548817+Racheljoness@users.noreply.github.com>
2024-10-24 21:04:40 -0400

    Update Transportation.java

--- Transportation.java
+++ Transportation.java
@@ -1,14 +1,14 @@
-
 public class Transportation {
     // instance variables
-    private String[] types = {"Air", "Land", "Water"};
-    private float cost;
+    private String name;
+    private double cost;
     private String purchaseMethod;
     private int averageSpeed;
     private int numPassengers;
 
     // constructor
-    Transportation(float cost, String purchaseMethod, int averageSpeed, int numPassengers){
+    Transportation(String name, double cost, String purchaseMethod, int averageSpeed, int numPassengers){
+        this.name = name;
         this.cost = cost;
         this.purchaseMethod = purchaseMethod;
         this.averageSpeed = averageSpeed;
@@ -17,27 +17,61 @@ public class Transportation {
 
 
     // accessors
-    public float getCost() { return cost; }
+    public String getName() { return name; }
+    public double getCost() { return cost; }
     public String getPurchaseMethod() { return purchaseMethod; }
     public int getAverageSpeed() { return averageSpeed; }
     public int getNumPassengers() { return numPassengers; }
 
     // mutators
+    public void setName(String name) { this.name = name; }
     public void setCost(float cost) {
         this.cost = cost;
     }
-
     public void setPurchaseMethod(String purchaseMethod) {
         this.purchaseMethod = purchaseMethod;
     }
-
     public void setAverageSpeed(int averageSpeed) {
         this.averageSpeed = averageSpeed;
     }
-
     public void setNumPassengers(int numPassengers) {
         this.numPassengers = numPassengers;
     }
 
+    // total cost method, if less than 1 system exits, if ticket multiplies cost by ticket, if rental multiplies
+    // number needed by ticket
+    public double totalCost(int passengers) {
+        if (passengers < 1) {
+            System.out.print("You must have at least one passenger");
+            System.exit(0);
+        }
+        if (purchaseMethod.equals("ticket")) {
+            if (getNumPassengers() < passengers){
+                System.out.printf("%n%s%d%n%s", "Max capacity is ", getNumPassengers(),
+                        "You cannot buy that many tickets!");
+                System.exit(0);
+            }
+            else{
+                return getCost() * passengers; }
+        }
+        if (purchaseMethod.equals("rental")) {
+            float ratio = (float) passengers / numPassengers;
+            int numberNeeded = (int) Math.ceil(ratio);
+            System.out.printf("%n%s%d%n", "You will need to rent ", numberNeeded);
+            return cost * numberNeeded;
+        }
+        else{
+            System.out.print("Not valid purchase method");
+            System.exit(0);
+            return 0;
+        }
+    }
+
+
+    // Origonal to string method for all classes
+    public String toString() {
+        return String.format("%-15s%10.2f%20s%20d%20d", getName(), getCost(), getPurchaseMethod(),
+                getAverageSpeed(), getNumPassengers());
+    }
 
 }
